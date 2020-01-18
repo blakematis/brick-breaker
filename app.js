f853c7dd574de15faee436f60b0155c71c362625
Blake <14blake1994@gmail.com>
2020-01-17 18:09:41 -0800

    changes var to const on imports

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var path = require('path');
+ const express = require('express');
+ const path = require('path');
 
-const app = express();
-const port = 8080;
+ const app = express();
+ const port = 8080;
 
 app.use(express.static('client'));
 
